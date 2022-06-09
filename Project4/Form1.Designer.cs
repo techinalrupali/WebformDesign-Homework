c188c85c7914226ddd1e6ef737c9018fa15b0203
techinalrupali <rupa99birajdar@gmail.com>
2022-06-09 21:30:18 +0530

    Add project files.

---
+++ Project4/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace Project4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblID = new System.Windows.Forms.Label();
+            this.lblCourseName = new System.Windows.Forms.Label();
+            this.lblFees = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtCourseName = new System.Windows.Forms.TextBox();
+            this.txtFees = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnShowAllDetails = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(34, 67);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(18, 13);
+            this.lblID.TabIndex = 0;
+            this.lblID.Text = "ID";
+            // 
+            // lblCourseName
+            // 
+            this.lblCourseName.AutoSize = true;
+            this.lblCourseName.Location = new System.Drawing.Point(34, 110);
+            this.lblCourseName.Name = "lblCourseName";
+            this.lblCourseName.Size = new System.Drawing.Size(71, 13);
+            this.lblCourseName.TabIndex = 1;
+            this.lblCourseName.Text = "Course Name";
+            // 
+            // lblFees
+            // 
+            this.lblFees.AutoSize = true;
+            this.lblFees.Location = new System.Drawing.Point(34, 154);
+            this.lblFees.Name = "lblFees";
+            this.lblFees.Size = new System.Drawing.Size(30, 13);
+            this.lblFees.TabIndex = 2;
+            this.lblFees.Text = "Fees";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(132, 67);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(125, 20);
+            this.txtID.TabIndex = 3;
+            // 
+            // txtCourseName
+            // 
+            this.txtCourseName.Location = new System.Drawing.Point(132, 110);
+            this.txtCourseName.Name = "txtCourseName";
+            this.txtCourseName.Size = new System.Drawing.Size(125, 20);
+            this.txtCourseName.TabIndex = 4;
+            // 
+            // txtFees
+            // 
+            this.txtFees.Location = new System.Drawing.Point(132, 154);
+            this.txtFees.Name = "txtFees";
+            this.txtFees.Size = new System.Drawing.Size(125, 20);
+            this.txtFees.TabIndex = 5;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(37, 223);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(167, 223);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 7;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(37, 298);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 8;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(167, 298);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(302, 56);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(310, 180);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // btnShowAllDetails
+            // 
+            this.btnShowAllDetails.Location = new System.Drawing.Point(333, 266);
+            this.btnShowAllDetails.Name = "btnShowAllDetails";
+            this.btnShowAllDetails.Size = new System.Drawing.Size(267, 23);
+            this.btnShowAllDetails.TabIndex = 11;
+            this.btnShowAllDetails.Text = "Show All Details";
+            this.btnShowAllDetails.UseVisualStyleBackColor = true;
+            this.btnShowAllDetails.Click += new System.EventHandler(this.btnShowAllDetails_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 407);
+            this.Controls.Add(this.btnShowAllDetails);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtFees);
+            this.Controls.Add(this.txtCourseName);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.lblFees);
+            this.Controls.Add(this.lblCourseName);
+            this.Controls.Add(this.lblID);
+            this.Name = "Form1";
+            this.Text = "Course Details";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Label lblCourseName;
+        private System.Windows.Forms.Label lblFees;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtCourseName;
+        private System.Windows.Forms.TextBox txtFees;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnShowAllDetails;
+    }
+}
+
---
+++ Project4/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Project4
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader dr;
+        public Form1()
+        {
+            InitializeComponent();
+            string strConnection = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            con = new SqlConnection(strConnection);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string str = "insert into Course_Details values(@CourseName,@Fees)";
+                cmd = new SqlCommand(str, con);
+                cmd.Parameters.AddWithValue("@CourseName", txtCourseName.Text);
+                cmd.Parameters.AddWithValue("@Fees", Convert.ToDouble(txtFees.Text));
+                con.Open();
+                int result = cmd.ExecuteNonQuery();
+                if (result==1)
+                {
+                    MessageBox.Show("Details Inserted Successfully");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string str = "update Course_Details set CourseName=@CourseName,Fees=@Fees where ID=@id";
+                cmd = new SqlCommand(str, con);
+                cmd.Parameters.AddWithValue("@id", Convert.ToInt32(txtID.Text));
+                cmd.Parameters.AddWithValue("@CourseName", txtCourseName.Text);
+                cmd.Parameters.AddWithValue("@Fees", Convert.ToDouble(txtFees.Text));
+                con.Open();
+                int result = cmd.ExecuteNonQuery();
+                if (result == 1)
+                {
+                    MessageBox.Show("Details Updated Successfully");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string str = "delete from Course_Details where ID=@id";
+                cmd = new SqlCommand(str, con);
+                cmd.Parameters.AddWithValue("@id", Convert.ToInt32(txtID.Text));
+                con.Open();
+                int result = cmd.ExecuteNonQuery();
+                if (result == 1)
+                {
+                    MessageBox.Show("Details Deleted Successfully");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string str = "select * from Course_Details where ID=@id";
+                cmd = new SqlCommand(str, con);
+                cmd.Parameters.AddWithValue("@id", Convert.ToInt32(txtID.Text));
+                con.Open();
+                dr = cmd.ExecuteReader();
+                if(dr.HasRows)
+                {
+                    while(dr.Read())
+                    {
+                        txtCourseName.Text = dr["CourseName"].ToString();
+                        txtFees.Text = dr["Fees"].ToString();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Details not found");
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void btnShowAllDetails_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string str = "select * from Course_Details";
+                cmd = new SqlCommand(str, con);
+                con.Open();
+                dr = cmd.ExecuteReader();
+                DataTable table = new DataTable();
+                table.Load(dr);
+                dataGridView1.DataSource = table;
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
