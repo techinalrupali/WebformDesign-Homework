37cbd702197122ca7408deadd44c4b288d0da27d
techinalrupali <rupa99birajdar@gmail.com>
2022-06-10 13:18:41 +0530

    commit

---
+++ Project4/Form2.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace Project4
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShowAllDetails = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtFees = new System.Windows.Forms.TextBox();
+            this.txtCourseName = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.lblFees = new System.Windows.Forms.Label();
+            this.lblCourseName = new System.Windows.Forms.Label();
+            this.lblID = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnShowAllDetails
+            // 
+            this.btnShowAllDetails.Location = new System.Drawing.Point(410, 303);
+            this.btnShowAllDetails.Name = "btnShowAllDetails";
+            this.btnShowAllDetails.Size = new System.Drawing.Size(267, 23);
+            this.btnShowAllDetails.TabIndex = 23;
+            this.btnShowAllDetails.Text = "Show All Details";
+            this.btnShowAllDetails.UseVisualStyleBackColor = true;
+            this.btnShowAllDetails.Click += new System.EventHandler(this.btnShowAllDetails_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(379, 93);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(310, 180);
+            this.dataGridView1.TabIndex = 22;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(244, 335);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 21;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(114, 335);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 20;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(244, 260);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 19;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(114, 260);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 18;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtFees
+            // 
+            this.txtFees.Location = new System.Drawing.Point(209, 191);
+            this.txtFees.Name = "txtFees";
+            this.txtFees.Size = new System.Drawing.Size(125, 20);
+            this.txtFees.TabIndex = 17;
+            // 
+            // txtCourseName
+            // 
+            this.txtCourseName.Location = new System.Drawing.Point(209, 147);
+            this.txtCourseName.Name = "txtCourseName";
+            this.txtCourseName.Size = new System.Drawing.Size(125, 20);
+            this.txtCourseName.TabIndex = 16;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(209, 104);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(125, 20);
+            this.txtID.TabIndex = 15;
+            // 
+            // lblFees
+            // 
+            this.lblFees.AutoSize = true;
+            this.lblFees.Location = new System.Drawing.Point(111, 191);
+            this.lblFees.Name = "lblFees";
+            this.lblFees.Size = new System.Drawing.Size(30, 13);
+            this.lblFees.TabIndex = 14;
+            this.lblFees.Text = "Fees";
+            // 
+            // lblCourseName
+            // 
+            this.lblCourseName.AutoSize = true;
+            this.lblCourseName.Location = new System.Drawing.Point(111, 147);
+            this.lblCourseName.Name = "lblCourseName";
+            this.lblCourseName.Size = new System.Drawing.Size(71, 13);
+            this.lblCourseName.TabIndex = 13;
+            this.lblCourseName.Text = "Course Name";
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(111, 104);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(18, 13);
+            this.lblID.TabIndex = 12;
+            this.lblID.Text = "ID";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnShowAllDetails);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtFees);
+            this.Controls.Add(this.txtCourseName);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.lblFees);
+            this.Controls.Add(this.lblCourseName);
+            this.Controls.Add(this.lblID);
+            this.Name = "Form2";
+            this.Text = "Disconnected Course Details";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShowAllDetails;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtFees;
+        private System.Windows.Forms.TextBox txtCourseName;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label lblFees;
+        private System.Windows.Forms.Label lblCourseName;
+        private System.Windows.Forms.Label lblID;
+    }
+}
---
+++ Project4/Form2.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace Project4
+{
+    public partial class Form2 : Form
+    {
+        SqlConnection con;
+        SqlDataAdapter da;
+        DataSet ds;
+        SqlCommandBuilder scb;
+        public Form2()
+        {
+            InitializeComponent();
+            string strConnection = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            con = new SqlConnection(strConnection);
+        }
+        private DataSet GetCourseData()
+        {
+            da = new SqlDataAdapter("select * from Course_Details", con);
+            da.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            scb = new SqlCommandBuilder(da);
+            ds = new DataSet();
+            da.Fill(ds, "course_details");
+            return ds;
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ds = GetCourseData();
+                DataRow row = ds.Tables["course_details"].NewRow();
+                row["CourseName"] = txtCourseName.Text;
+                row["Fees"] = txtFees.Text;
+                ds.Tables["course_details"].Rows.Add(row);
+                int result = da.Update(ds.Tables["course_details"]);
+                if(result==1)
+                {
+                    MessageBox.Show("Course Details Inserted");
+
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                ds = GetCourseData();
+                DataRow row = ds.Tables["course_details"].Rows.Find(txtID.Text);
+                if (row != null)
+                {
+                    row["CourseName"] =txtCourseName.Text;
+                    row["Fees"] = txtFees.Text;
+                    int result = da.Update(ds.Tables["course_details"]);
+                    if (result == 1)
+                    {
+                        MessageBox.Show("Course Record Updated");
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Id does not exists to update");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ds = GetCourseData();
+                DataRow row = ds.Tables["course_details"].Rows.Find(txtID.Text);
+                if (row != null)
+                {
+                    row.Delete();//delete the row from dataset
+                    int result = da.Update(ds.Tables["course_details"]);
+                    if (result == 1)
+                    {
+                        MessageBox.Show("Course Record Deleted");
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Id does not exists to Delete");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ds = GetCourseData();
+                DataRow row = ds.Tables["course_details"].Rows.Find(txtID.Text);
+                if (row != null)
+                {
+                    txtCourseName.Text = row["CourseName"].ToString();
+                    txtFees.Text = row["Fees"].ToString();
+
+                }
+                else
+                {
+                    MessageBox.Show("Course Record does not exists ");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnShowAllDetails_Click(object sender, EventArgs e)
+        {
+            ds = GetCourseData();
+            dataGridView1.DataSource = ds.Tables["course_details"];
+        }
+    }
+}
